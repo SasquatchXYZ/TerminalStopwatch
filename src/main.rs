d37ea0b2f81cdab81664af4d4358d20cfa40113a
Dalton Ricker <40616938+SasquatchXYZ@users.noreply.github.com>
2024-09-28 22:29:13 -0400

    Developing the code

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,9 @@
-use std::io::stdout;
+use std::{
+    io::stdout,
+    time::Instant,
+};
+
+use crossterm::event::{poll, read, Event, KeyCode, KeyEventKind};
 use ratatui::{
     backend::CrosstermBackend,
     layout::{Constraint, Direction, Layout},
@@ -6,12 +11,73 @@ use ratatui::{
     Terminal,
 };
 
+struct Stopwatch {
+    now: Instant,
+    state: StopwatchState,
+    display: String,
+}
+
+enum StopwatchState {
+    NotStarted,
+    Running,
+    Done,
+}
+
+impl Stopwatch {
+    fn new() -> Self {
+        Self {
+            now: Instant::now(),
+            state: StopwatchState::NotStarted,
+            display: String::from("0:00:00"),
+        }
+    }
+    fn get_time(&self) -> String {
+        use StopwatchState::*;
+        match self.state {
+            NotStarted => String::from("0:00:00"),
+            Running => {
+                let mut elapsed = self.now.elapsed().as_millis();
+                let minutes = elapsed / 60000;
+                elapsed -= minutes * 60000;
+                let seconds = elapsed / 1000;
+                elapsed -= seconds * 1000;
+                let split_seconds = elapsed / 10;
+                format!("{minutes}:{seconds}:{split_seconds}")
+            }
+            Done => self.display.clone(),
+        }
+    }
+    fn next_state(&mut self) {
+        use StopwatchState::*;
+        match self.state {
+            NotStarted => {
+                self.now = Instant::now();
+                self.state = Running;
+            }
+            Running => {
+                self.display = self.get_time();
+                self.state = Done;
+            }
+            Done => self.state = NotStarted,
+        }
+    }
+}
+
 fn main() {
     let stdout = stdout();
     let backend = CrosstermBackend::new(stdout);
     let mut terminal = Terminal::new(backend).unwrap();
+    let mut stopwatch = Stopwatch::new();
 
     loop {
+        if poll(std::time::Duration::from_millis(0)).unwrap() {
+            if let Event::Key(key_event) = read().unwrap() {
+                if let (KeyCode::Enter, KeyEventKind::Press) = (key_event.code, key_event.kind) {
+                    stopwatch.next_state();
+                }
+            }
+        }
+
         terminal
             .draw(|f| {
                 let layout = Layout::default()
@@ -24,8 +90,11 @@ fn main() {
                 let stopwatch_block = Block::default().title("Stopwatch").borders(Borders::ALL);
                 let utc_time_block = Block::default().title("UTC Time").borders(Borders::ALL);
 
-                let stopwatch_text = Paragraph::new("I'm a stopwatch").block(stopwatch_block);
-                let utc_text = Paragraph::new("Hi I'm in London").block(utc_time_block);
+                let stopwatch_text = Paragraph::new(stopwatch.get_time())
+                    .block(stopwatch_block);
+                let utc_text = Paragraph::new(
+                    chrono::offset::Utc::now().format("%Y/%m/%d %H:%M:%S").to_string()
+                ).block(utc_time_block);
 
                 f.render_widget(stopwatch_text, stopwatch_area);
                 f.render_widget(utc_text, utc_time_area);
