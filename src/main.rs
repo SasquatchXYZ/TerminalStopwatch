ee7549f297e49415e740dc9411950d6f40643ed5
Dalton Ricker <40616938+SasquatchXYZ@users.noreply.github.com>
2024-09-28 21:48:09 -0400

    Setup and first code

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,37 @@
+use std::io::stdout;
+use ratatui::{
+    backend::CrosstermBackend,
+    layout::{Constraint, Direction, Layout},
+    widgets::{Block, Borders, Paragraph},
+    Terminal,
+};
+
 fn main() {
-    println!("Hello, world!");
+    let stdout = stdout();
+    let backend = CrosstermBackend::new(stdout);
+    let mut terminal = Terminal::new(backend).unwrap();
+
+    loop {
+        terminal
+            .draw(|f| {
+                let layout = Layout::default()
+                    .direction(Direction::Horizontal)
+                    .constraints([Constraint::Percentage(50), Constraint::Percentage(50)])
+                    .split(f.area());
+                let stopwatch_area = layout[0];
+                let utc_time_area = layout[1];
+
+                let stopwatch_block = Block::default().title("Stopwatch").borders(Borders::ALL);
+                let utc_time_block = Block::default().title("UTC Time").borders(Borders::ALL);
+
+                let stopwatch_text = Paragraph::new("I'm a stopwatch").block(stopwatch_block);
+                let utc_text = Paragraph::new("Hi I'm in London").block(utc_time_block);
+
+                f.render_widget(stopwatch_text, stopwatch_area);
+                f.render_widget(utc_text, utc_time_area);
+            })
+            .unwrap();
+        std::thread::sleep(std::time::Duration::from_millis(20));
+        terminal.clear().unwrap();
+    }
 }
